5b7d3c1e69805d7b42d9d12ff746b0c14862af68
bibliopelis <bibliopelis@gmail.com>
2019-10-26 18:32:51 -0300

    mi primer commit en bibliopelis

--- biblio_pelis/bibliocatalogo/admin.py
+++ biblio_pelis/bibliocatalogo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import user
+from .models import pelicula
 # Register your models here.
+admin.site.register(user)
+admin.site.register(pelicula)
--- biblio_pelis/bibliocatalogo/views.py
+++ biblio_pelis/bibliocatalogo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import pelicula, user
 
+def index (request):
 # Create your views here.
+    num_users = user.objects.all().count()
+    return render(request, 'index.html', context={'num_books':num_users})
