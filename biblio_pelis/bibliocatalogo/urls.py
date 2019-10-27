3593fffc5b886aede9417a2ad9ad9c7e395af31f
bibliopelis <bibliopelis@gmail.com>
2019-10-27 18:29:11 -0300

    paginas

--- biblio_pelis/bibliocatalogo/urls.py
+++ biblio_pelis/bibliocatalogo/urls.py
@@ -5,4 +5,5 @@ from .views import index, view_crear_usuario
 urlpatterns = [
     path('', index,name='index'),
     path('', view_crear_usuario, name='registro')
+    
 ]
--- biblio_pelis/bibliocatalogo/views.py
+++ biblio_pelis/bibliocatalogo/views.py
@@ -1,5 +1,8 @@
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import pelicula, user
+from django.views import generic
 from . import forms
 def index (request):
 # Create your views here.
@@ -14,3 +17,19 @@ def view_crear_usuario(request):
         'formaReg': form
     }
     return render(request, 'registro', context)
+
+class creacion_usuario(CreateView):
+    model=user
+    fields='__all__'
+
+class Actualizacion_usuario(UpdateView):
+    model=user
+    fields=['nombre','email','password']  
+
+class Eliminacion_usuario(DeleteView):
+    model=user
+    succes_url = reverse_lazy('user')
+
+class DetalleVistaUsuario(generic.DetailView):
+    model=user
+
