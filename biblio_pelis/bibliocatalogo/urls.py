25218ed15730cdb03abf38ee13019f27e5d5d763
zeky <edisonsepulvedagarrido@gmail.com>
2019-10-28 00:43:02 -0300

    commit new branch

--- biblio_pelis/bibliocatalogo/urls.py
+++ biblio_pelis/bibliocatalogo/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', index,name='index'),
-    path('', view_crear_usuario, name='registro')
-    path('user/<int:pk>',views.DetalleVistaUsuario.as_view(),name='user-detail')
-]
-urlpatterns += [
-    path('user/create/',views.creacion_usuario.as_view(),name='user_create'),
-    path('user/<int:pk>/update/',views.Actualizacion_usuario.as_view(),name='user_update'),
-    path('user/<int:pk>/delete/',views.Eliminacion_usuario.as_view(),name='user_delete'),
+    path('', views.index ,name='index')
     
 ]
 
+urlpatterns += [
+    path('registro/', views.creacion_usuario.as_view(), name='user_form')
+    
+]
